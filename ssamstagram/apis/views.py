b78f9b1210f5f0b51a48f393c08df17649d82b0a
ssamko <teacher.ssamko@gmail.com>
2020-05-10 14:03:31 +0900

    User API created

--- ssamstagram/apis/views.py
+++ ssamstagram/apis/views.py
@@ -10,3 +10,18 @@ class BaseView(View):
             'message': message,
         }
         return JsonResponse(result, status)
+
+
+class UserCreateView(BaseView):
+    @method-decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request):
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        email = request.POST.get('email', '')
+
+        user = User.objects.create_user(username, email, password)
+
+        return self.response({'user.id': user.id})
