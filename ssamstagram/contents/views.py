e812ce07828c28a9552326cbc69e70b04ecdf84c
ssamko <teacher.ssamko@gmail.com>
2020-05-10 18:57:46 +0900

    login page detached

--- ssamstagram/contents/views.py
+++ ssamstagram/contents/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 class HomeView(TemplateView):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(HomeView, self).dispatch(request, *args, **kwargs)
+
     template_name = 'home.html'
